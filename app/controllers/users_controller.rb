8b390809bac507cc6a913b065c40af07c2558d55
HouseStark <jmullin@smu.edu>
2015-10-20 21:37:30 -0500

    revert to before rake test errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,24 +5,24 @@ class UsersController < ApplicationController
   end
 
   def new
-  		@user = User.new		#@user?
+      @user = User.new    #@user?
   end
 
   def create
-  		@user = User.new(user_params)
-  		if @user.save
+      @user = User.new(user_params)
+      if @user.save
         log_in @user
-  			flash[:success] = "Welcome to the Sample App!"
-  			redirect_to user_url(@user)
-  		else 
-  			render 'new'
-  		end
-  	end
+        flash[:success] = "Welcome to the Sample App!"
+        redirect_to user_url(@user)
+      else 
+        render 'new'
+      end
+    end
 
-  	private
-  		def user_params
-  			params.require(:user).permit(:name, :email, :password, 
-  												:password_confirmation)
-  		end
+    private
+      def user_params
+        params.require(:user).permit(:name, :email, :password, 
+                          :password_confirmation)
+      end
 
 end
